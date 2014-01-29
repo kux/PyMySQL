05f1aa962e1138f6203b47152a4036e522f3a7df
Cucu <alexandruioan.cucu@gmail.com>
2014-01-29 16:59:40 +0000

    Add support for bulk inserts in Cursor.executemany

--- pymysql/cursors.py
+++ pymysql/cursors.py
@@ -79,6 +79,18 @@ class Cursor(object):
         self._do_get_result()
         return True
 
+    def _get_escaped_args(self, conn, args):
+        if isinstance(args, (tuple, list)):
+            escaped_args = tuple(conn.escape(arg) for arg in args)
+        elif isinstance(args, dict):
+            escaped_args = dict((key, conn.escape(val))
+                                for (key, val) in args.items())
+        else:
+            #If it's not a dictionary let's try escaping it anyways.
+            #Worst case it will throw a Value error
+            escaped_args = conn.escape(args)
+        return escaped_args
+
     def execute(self, query, args=None):
         ''' Execute a query '''
         conn = self._get_db()
@@ -89,26 +101,40 @@ class Cursor(object):
         # TODO: make sure that conn.escape is correct
 
         if args is not None:
-            if isinstance(args, (tuple, list)):
-                escaped_args = tuple(conn.escape(arg) for arg in args)
-            elif isinstance(args, dict):
-                escaped_args = dict((key, conn.escape(val)) for (key, val) in args.items())
-            else:
-                #If it's not a dictionary let's try escaping it anyways.
-                #Worst case it will throw a Value error
-                escaped_args = conn.escape(args)
-            query = query % escaped_args
+            query = query % self._get_escaped_args(conn, args)
 
         result = self._query(query)
         self._executed = query
         return result
 
-    def executemany(self, query, args):
+    def executemany(self, query, seq_of_args):
         ''' Run several data against one query '''
-        if not args:
+        if not seq_of_args:
             return
 
-        self.rowcount = sum(self.execute(query, arg) for arg in args)
+        re_insert = re.compile(r'insert\s+into', re.I)
+        re_insert_values = insert_values
+        if re.match(re_insert, query):
+            re_comment = re.compile(r"\/\*.*\*\/")
+            re_on_duplicate = re.compile(r'\s*on duplicate key.*$', re.I)
+            # remove:
+            # 1. comments
+            # 2. everything after 'on duplicate key'
+            # as these might cause re_insert_values to match
+            # more than it has to
+            tmp = re.sub(re_on_duplicate, '', re.sub(re_comment, '', query))
+            matches = re.search(re_insert_values, tmp)
+            if not matches:
+                raise ValueError("Invalid SQL syntax")
+            conn = self._get_db()
+            values = [matches.group(1) % self._get_escaped_args(conn, args)
+                      for args in seq_of_args]
+            query = query.replace(matches.group(1), ','.join(values), 1)
+            result = self._query(query)
+            self._executed = query
+            return result
+
+        self.rowcount = sum(self.execute(query, arg) for arg in seq_of_args)
         return self.rowcount
 
     def callproc(self, procname, args=()):
--- pymysql/tests/test_basic.py
+++ pymysql/tests/test_basic.py
@@ -1,3 +1,5 @@
+import pymysql.cursors
+
 from pymysql.tests import base
 from pymysql import util
 
@@ -12,7 +14,7 @@ class TestConversion(base.PyMySQLTestCase):
         c.execute("create table test_datatypes (b bit, i int, l bigint, f real, s varchar(32), u varchar(32), bb blob, d date, dt datetime, ts timestamp, td time, t time, st datetime)")
         try:
             # insert values
-            v = (True, -3, 123456789012, 5.7, "hello'\" world", u"Espa\xc3\xb1ol", "binary\x00data".encode(conn.charset), datetime.date(1988,2,2), datetime.datetime.now(), datetime.timedelta(5,6), datetime.time(16,32), time.localtime())
+            v = (True, -3, 123456789012, 5.7, "hello'\" world", u"Espa\xc3\xb1ol", "binary\x00data".encode(conn.charsent), datetime.date(1988,2,2), datetime.datetime.now(), datetime.timedelta(5,6), datetime.time(16,32), time.localtime())
             c.execute("insert into test_datatypes (b,i,l,f,s,u,bb,d,dt,td,t,st) values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", v)
             c.execute("select b,i,l,f,s,u,bb,d,dt,td,t,st from test_datatypes")
             r = c.fetchone()
@@ -207,7 +209,89 @@ class TestCursor(base.PyMySQLTestCase):
             c.execute("drop table mystuff")
 
 
-__all__ = ["TestConversion","TestCursor"]
+class TestBulkInserts(base.PyMySQLTestCase):
+
+    cursor_type = pymysql.cursors.DictCursor
+
+    def setUp(self):
+        super(TestBulkInserts, self).setUp()
+        self.conn = conn = self.connections[0]
+        c = conn.cursor(self.cursor_type)
+
+        # create a table ane some data to query
+        c.execute("drop table if exists bulkinsert")
+        c.execute(
+"""CREATE TABLE bulkinsert
+(
+id int(11),
+name char(20),
+age int,
+height int,
+PRIMARY KEY (id)
+)
+""")
+
+    def _verify_records(self, data):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        cursor.execute("SELECT id, name, age, height from bulkinsert")
+        result = cursor.fetchall()
+        self.assertEqual(len(data), len(result))
+        self.assertItemsEqual(data, result)
+
+    def test_bulk_insert(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
+        cursor.executemany("insert into bulkinsert (id, name, age, height) "
+                           "values (%s,%s,%s,%s)", data)
+        cursor.execute('commit')
+        self._verify_records(data)
+
+    def test_bulk_insert_single_record(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        data = [(0, "bob", 21, 123)]
+        cursor.executemany("insert into bulkinsert (id, name, age, height) "
+                           "values (%s,%s,%s,%s)", data)
+        cursor.execute('commit')
+        self._verify_records(data)
+
+    def test_bulk_insert_comments_ignored(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
+        cursor.executemany("insert into bulkinsert (id, name, age, height) "
+                           "values (%s, %s,  %s,  %s) /* %s, %s) */", data)
+        cursor.execute('commit')
+        self._verify_records(data)
+
+    def test_bulk_insert_duplicate_key(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
+        cursor.executemany("insert into bulkinsert (id, name, age, height) "
+                           "values (%s, %s,  %s, %s) ON DUPLICATE KEY UPDATE "
+                           "height=VALUES(age)", data)
+        cursor.execute('commit')
+        self._verify_records(data)
+
+    def test_bulk_insert_duplicate_key_actually_have_duplicates(self):
+        conn = self.connections[0]
+        cursor = conn.cursor()
+        data = [(0, "bob", 21, 123), (1, "jim", 56, 45), (2, "fred", 100, 180)]
+        cursor.executemany("insert into bulkinsert (id, name, age, height) "
+                           "values (%s, %s,  %s, %s)", data)
+        cursor.executemany("insert into bulkinsert (id, name, age, height) "
+                           "values (%s, %s,  %s, %s) ON DUPLICATE KEY UPDATE "
+                           "height=VALUES(age)", data)
+        cursor.execute('commit')
+        expected_update = [(0, "bob", 21, 21), (1, "jim", 56, 56),
+                           (2, "fred", 100, 100)]
+        self._verify_records(expected_update)
+
+
+__all__ = ["TestConversion","TestCursor","TestBulkInserts"]
 
 if __name__ == "__main__":
     import unittest
